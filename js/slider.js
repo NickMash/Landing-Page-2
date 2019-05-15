c6dcb16535422f03fdcb67f1c55706e3f6472f03
Your Name <nicholasgrand@gmail.com>
2019-05-16 00:48:45 +0300

    New Landing Page

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+    let element = document.getElementById('slider-container');
+
+    element.addEventListener('click', function(){
+        element.style.background='url()';
+    });
+});
